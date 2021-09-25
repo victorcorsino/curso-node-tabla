c9aed336daf009a13cf4f8d50868d43e85a5d2d6
Victor Corsino <vcorsinoa@gmail.com>
2021-09-25 18:43:19 -0400

    Primer commit

---
+++ app.js
@@ -0,0 +1,22 @@
+
+const { crearArchivo } = require('./helpers/multiplicar');
+const argv = require('./config/yargs');
+require('colors');
+
+console.clear();
+
+// console.log(process.argv);
+// console.log(argv);
+
+// console.log('base: yargs', argv.base);
+
+// console.log(proccess.argv);
+// const [,,arg3 = 'base=5'] = process.argv;
+// const [, base = 5 ] = arg3.split('=');
+// console.log(base);
+
+// const base = 5;
+
+crearArchivo(argv.b, argv.l, argv.h)
+    .then( nombreArchivo => console.log(nombreArchivo.rainbow, 'creado'))
+    .catch( err => console.log(err) );
